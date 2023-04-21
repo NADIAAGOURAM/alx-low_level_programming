1a30b33ddaad6c2d6b015cc500c6e55d98158e3f
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-04-21 09:32:44 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,12 +7,12 @@
 
 int main(void)
 {
-        char alphabet;
+	char alphabet;
 
-        for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
-                putchar(alphabet);
-       	for (alphabet = 'A' ; alphabet <= 'Z'; alphabet++)
-                putchar(alphabet);
+	for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
+		putchar(alphabet);
+	for (alphabet = 'A' ; alphabet <= 'Z'; alphabet++)
+		putchar(alphabet);
 	putchar('\n');
-        return (0);
+	return (0);
 }
