12306b9a9b0a04f85ff97cc28aa2c2bfb75779f5
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-04-25 12:54:54 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,11 +10,11 @@ int main(void)
 char ch[8] = "_putchar";
 int i = 0;
 
-while(i < 8)
-	{
-		_putchar(ch[i]);
-		i++;
-	}
+while (i < 8)
+{
+	_putchar(ch[i]);
+	i++;
+}
 _putchar('\n');
 return (0);
 }
