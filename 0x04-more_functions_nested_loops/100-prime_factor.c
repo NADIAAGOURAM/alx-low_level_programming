5a320069a3fb0a576809fc800324091248b48e46
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-04-27 15:29:15 -0700

    more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prime numbers
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long int x = 612852475143;
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+
+/**
+ * print_number - prints numbers
+ * @n: number to be printed
+ * Return:void
+ */
+
+void print_number(int n)
+{
+}
