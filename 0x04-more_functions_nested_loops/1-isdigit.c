3c5b1f8d008cb585a503951300fb952502537591
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-04-27 13:03:09 -0700

    more_functions_nested_loops

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * _isdigit - checks for a digit (0 through 9).
- * @c : character to check
- * Return : 0 or 1
+ * _isdigit - check if numbers are 0 - 9
+ * @c: char to be checked
+ *
+ * Return: 0 or 1
  */
 
 int _isdigit(int c)
@@ -13,10 +14,4 @@ int _isdigit(int c)
 	else
 		return (0);
 }
-
-
-
-
-
-
 
