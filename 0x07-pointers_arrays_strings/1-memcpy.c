9654f2e3585a398d6de0dcc9f282a2d7271040a8
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-05-08 13:32:47 -0700

    C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_memcpy - copies memory area.
+ * @dest:char
+ * @src:char
+ * @n:unsigned int bytes
+ * Return : dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src [i];
+	}
+	return (dest);
+}
