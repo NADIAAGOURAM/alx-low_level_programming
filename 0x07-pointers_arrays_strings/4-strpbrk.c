fb16eb38b6ad3760094479afc265a6079b892240
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-05-08 15:10:02 -0700

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,15 +11,15 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i;
 
-		while (*s)
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
 		{
-			for (i = 0; accept[i]; i++)
-			{
-				if (*s == accept[i])
+			if (*s == accept[i])
 				return (s);
-			}
-		s++;
 		}
+	s++;
+	}
 
 	return (NULL);
 
