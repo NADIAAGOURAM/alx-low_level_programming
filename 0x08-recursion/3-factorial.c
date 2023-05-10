a74ebdd9db063187ce595fbe611fab0eaae45deb
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-05-10 05:17:21 -0700

    C - Recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,15 +8,13 @@
 
 int factorial(int n)
 {
-	int fact = 1;
 
 	if (n < 0)
 		return (-1);
-	else if (n == 0)
+	if (n == 0)
 		return (1);
 	else
-	{	
-		fact *= factorial(n - 1);
-		return (fact);
+	{
+		return (n * factorial(n - 1));
 	}
 }
