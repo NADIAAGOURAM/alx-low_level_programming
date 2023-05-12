b1a1872de9835ca6aacc07f72966c0f49976f901
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-05-12 15:51:51 -0700

    C - argc, argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,17 +5,21 @@
 
 /**
  * main - adds positive numbers
- * @argc:number of arguments
- * @argv:array of arguments
- *
+ * @argc: n args
+ * @argv: arr args
  * Return: 0
  */
-
 int main(int argc, char *argv[])
 {
-	int i, sum;
+	unsigned int i, sum, num;
 
 	sum = 0;
+
+	if (argc < 3)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
 	while (argc-- && argc > 0)
 	{
 		for (i = 0; argv[argc][i] != '\0'; i++)
@@ -26,14 +30,9 @@ int main(int argc, char *argv[])
 				return (1);
 			}
 		}
-		sum += atoi(argv[argc]);
-	}
-	if (argc < 3)
-	{
-		printf("%d\n", 0);
-		return (0);
+		num = atoi(argv[argc]);
+		sum += num;
 	}
 	printf("%d\n", sum);
 	return (sum);
 }
-
