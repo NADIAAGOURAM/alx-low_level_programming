bf062c3b5d7b0526df71b2b9316ea5f5352000be
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-05-12 15:16:16 -0700

    C - argc, argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+int _atoi(char *s);
+
+/**
+ * _atoi - convert a string to an integer.
+ * @s: char type string
+ * Return: integer converted
+ */
+
+int _atoi(char *s)
+{
+	unsigned int num = 0;
+	int sign = 1;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+	return (num * sign);
+}
 
 /**
  * main - multiplies two numbers
