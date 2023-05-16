c27eaa785e5e7c1a090b44948aa802c485f3bc92
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-05-16 05:38:24 -0700

    C - malloc, free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -32,24 +32,24 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2;
 
 	if (!s1)
-		s1 = "";
+		s1 = '\0';
 	else
 		len1 = _strlen(s1);
 
 	if (!s2)
-		s2 = "";
+		s2 = '\0';
 	else
 		len2 = _strlen(s2);
 	concat = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (concat == NULL)
 		return (NULL);
 	len1 = len2 = 0;
-	while(s1[len1] !='\0')
-	{	
+	while(s1[len1] != '\0')
+	{
 		concat[len1] = s1[len1];
 		len1++;
 	}
-	while(s2[len2] !='\0')
+	while(s2[len2] != '\0')
 	{
 		concat[len1] = s2[len2];
 		len1++;
