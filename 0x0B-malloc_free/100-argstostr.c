dea74c0ca6d7e236f29160d742a0db422a06bf7f
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-05-16 12:29:54 -0700

    C - malloc, free

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: int
+ * @av: double pointer array
+ * Return: the args one line at a time
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *s;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	return (s);
+}
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * **strtow - splits a string into words
+ * @str: string to split
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
+ */
+
+char **strtow(char *str)
+{
+}
