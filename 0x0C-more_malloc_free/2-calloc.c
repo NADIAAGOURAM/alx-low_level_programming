a1d6a4399f138f595c20d4a9e6910ef48f4014da
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-05-18 15:01:59 -0700

    C - More malloc, free

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * *_memset - fills memory with a constant byte.
+ * @s:char
+ * @b:char
+ * @n:unsigned int
+ * Return:char
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
+
+/**
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb:elements
+ * @size:bytes
+ * Return:pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size);
+{
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+	_memset(ptr, 0, nmemb * size);
+
+	return (ptr);
+}
+
