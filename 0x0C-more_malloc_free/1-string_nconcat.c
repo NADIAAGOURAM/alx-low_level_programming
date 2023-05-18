3d24a914f452b57e116bbf7424e0f9d45aa217b0
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-05-18 14:28:40 -0700

    C - More malloc, free

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+int _strlen(char *s);
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/**
+ * string_nconcat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ * @n:bytes of s2
+ * Return: char pointer
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *concat;
+	int len1, len2, i, j;
+
+	if (s1 == NULL)
+		s1 = "\0";
+	else
+		len1 = _strlen(s1);
+
+	if (s2 == NULL)
+		s2 = "\0";
+	else
+		len2 = _strlen(s2);
+	if (n > len2)
+	n = len2;
+	concat = malloc(len1 + n + 1);
+	if (concat == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		concat[i] = s1[i];
+		i++;
+	}
+	for (j = 0; j < n; j++)
+	{
+		concat[i] = s2[j];
+		i++;
+	}
+	concat[i] = '\0';
+	return (concat);
+}
