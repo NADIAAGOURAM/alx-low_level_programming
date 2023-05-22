5f40104de7d15e0d33be41230bb00ca4541095fd
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-05-22 14:29:46 -0700

    C - Structures, typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,13 +15,9 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)\n");
-		/**
-		 * if (d->age)
-		 */
+	
 		printf("Age: %f\n", d->age);
-		/**
-		 * else
-		 */
+		
 		if (d->owner)
 			printf("Owner: %s\n", d->owner);
 		else
