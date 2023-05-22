ddecfa4acb9aae033df2c9142e8b5f7e130977e8
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-05-22 13:06:33 -0700

    C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - poppy
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description:poppy's information
+ */
+
 struct dog
 {
 	char *name;
