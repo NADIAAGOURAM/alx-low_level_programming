a532d28b2e95b1532afeb89f00ddf7d1cf8bdd23
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-05-24 13:09:05 -0700

    C - Function pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,10 +17,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		for (i = 0 ; i < size ; i++)
 		{
-			if ((*cmp)array[i] != 0)
+			if ((*cmp)(array[i]) != 0)
 				return (i);
 		}
-	else
-		return (-1);
 	}
+	return (-1);
 }
