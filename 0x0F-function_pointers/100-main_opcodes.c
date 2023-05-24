31faf023d946c3391ac369f17fef8b5a9003d512
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-05-24 15:08:39 -0700

    C - Function pointers

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ *main -  prints the opcodes of its own main function
+ *@argc:number of arguments
+ *@argv:array of arguments
+ *Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+}
+
