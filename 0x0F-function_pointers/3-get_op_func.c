40a9fde25de6e5b81406cd13151fc077cdbbcf88
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-05-24 14:35:25 -0700

    C - Function pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,20 +3,22 @@
 #include <stdio.h>
 
 /**
- * get_op_func -selects the correct function to perform the operation asked by the user.
- * @s: The operator passed as argument.
- * Return:a pointer to the function that corresponds to the operator given as a parameter
+ * get_op_func -selects the correct function to perform
+ * the operation asked by the user.
+ * @s: The operator passed as argument
+ * Return:a pointer to the function that corresponds to the operator
+ * given as a parameter
  */
 
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
 	};
 	int i;
 
