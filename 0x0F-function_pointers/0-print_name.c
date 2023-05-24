be03839207b794ea5a7c03d5ba69427cc3d26d94
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-05-24 04:32:34 -0700

    C - Function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stdio.h>
 /**
  * print_name - prints a name
  * @name: name of the person
@@ -14,5 +14,4 @@ void print_name(char *name, void (*f)(char *))
 	{
 		f(name);
 	}
-
 }
