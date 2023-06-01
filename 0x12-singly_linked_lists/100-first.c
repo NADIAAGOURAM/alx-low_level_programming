022bf54c2f1a576536a0288c0a73a5a13453d88e
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-06-01 15:19:29 -0700

    C - Singly linked lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,8 @@ void print_first(void) __attribute__((constructor));
  * print_first - prints a sentence before the main
  *
  */
-void print_first(void) {
-    printf("You're beat! and yet, you must allow,\n");
-    printf("I bore my house upon my back!\n");
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
