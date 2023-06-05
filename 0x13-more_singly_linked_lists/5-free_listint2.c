63b266b86b76c4de1c65ae1c83fef44aeac2dab1
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-06-05 15:48:56 -0700

    C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list.
+ * @head: list to be freed
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *newnode;
+
+	while (*head != NULL)
+	{
+		newnode = (*head)->next;
+		free(*head);
+		*head = newnode;
+	}
+}
