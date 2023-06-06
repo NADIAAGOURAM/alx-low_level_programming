9dccab76fca0bc754014d5955b13ba5ceca3618f
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-06-06 06:34:58 -0700

    C - More singly linked lists

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,13 +10,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	if (*head == NULL)
+	if (head == NULL)
 	       return (NULL);	
-	for (i = 0; *head != NULL; i++)
-	{
-		if (i != index)
-			head = head->next;
-		else
-			return (head->n);
-	}
+	for (i = 0; i < index; i++)
+		head = head->next;
+	return (head);
 }
