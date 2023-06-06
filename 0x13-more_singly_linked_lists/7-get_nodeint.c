422669e4cf85b219fbee000f2cf3449810c13b12
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-06-06 06:26:47 -0700

    C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of a list
+ * @head: double pointer.
+ * @index:index of the node, starting at 0
+ * Return: returns the nth node of a list or NULL
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	if (head == NULL)
+	       return (NULL);	
+	for (i = 0; head != NULL; i++)
+	{
+		if (i != index)
+			head = head->next;
+		else
+			return (head->n);
+	}
+}
