34d87ba3ecfed3625d410ccf8768b00747dc3fcd
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-06-12 12:15:44 -0700

    C - File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /*
- * read_textfile -  reads a text file and prints it to the POSIX standard output.
- * @ilename: The name of the file.
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: The name of the file.
  * @letters: The number of letters it should read and print
  * return:the actual number of letters it could read and print or 0 on failure.
  */
