734a359738b8a044b8728ab63c24fb93e9c32c08
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-07-20 11:17:36 -0700

    commit

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -26,4 +26,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		current = current->next;
 	if (current == NULL)
 		return (-1);
-}
+	if (current->prev != NULL)
+		current->prev->next = current->next;
+	if (current->next != NULL)
+		current->next->prev = current->prev;
+	free(current);
+	return (1);
