b9980b1fa71cfe5fc23813766ee516f7b4918574
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-07-20 10:57:27 -0700

    commit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-	
+
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
@@ -44,5 +44,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	current->next = new_node;
 	new_node->prev = current;
 
-	return new_node;
+	return (new_node);
 }
---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - Deletes a node at a given in
+ * @head: Pointer to the head
+ * @index: Index of the node
+ * Return: 1 upon success, -1
+ **/
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+}
