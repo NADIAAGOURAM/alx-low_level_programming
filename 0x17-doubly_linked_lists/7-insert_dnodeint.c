47adef799e74e654182ba0362c7e639dd91c649e
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-07-21 15:41:06 -0700

    commit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,40 +9,32 @@
 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *current;
-	unsigned int count = 0;
+	dlistint_t *new_node, *current = h;
+	unsigned int i = 0;
 
-	if (h == NULL)
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	while (current != NULL && i < idx - 1)
+	{
+		current = current->next;
+		i++;
+	}
+
+	if (current == NULL)
 		return (NULL);
+	if (current->next == NULL)
+		return (add_dnodeint_end(h, n));
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = NULL;
-	if (idx == 0 && *h == NULL)
-	{
-		*h = new_node;
-		return (new_node);
-	}
-	current = *h;
-	while (current != NULL && count < idx - 1)
-	{
-		current = current->next;
-		count++;
-	}
-	if (current == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
 	new_node->next = current->next;
-	if (current->next != NULL)
-		current->next->prev = new_node;
-	current->next = new_node;
 	new_node->prev = current;
+	current->next->prev = new_node;
+	current->next = new_node;
 
 	return (new_node);
 }
